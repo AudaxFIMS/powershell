469f9b3548fefd2d3a5c146867f7a711479e4af8
Alexey Semeshin <alexey_semeshin@epam.com>
2019-11-14 15:28:11 -0500

    Script with App start and restart if it's down

---
+++ src/process_start_script_and_restart_if_process_down.ps1
@@ -0,0 +1,17 @@
+﻿$target = 'nginx'
+$workdir = "c:\nginx"
+
+function startNginx {
+    echo "Start Nginx"
+
+    $processes = Get-Process | Where-Object {$_.ProcessName -eq $target}
+    Foreach($process in $processes) {
+        $process.Kill();
+    }
+
+    $process = Start-Process -FilePath "nginx.exe" -WorkingDirectory "$workdir" -PassThru -WindowStyle Hidden -Wait
+
+    startNginx
+}
+
+startNginx
